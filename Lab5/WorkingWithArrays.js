c2cb0d91aeee27a943cc82b347ba552e16ecd466
karangoyal03 <goyalkaran31@gmail.com>
2024-07-17 17:59:25 -0400

    changes for lab routes

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -60,20 +60,20 @@ export default function WorkingWithArrays(app) {
   });
 
   app.get("/lab5/todos/:id/delete", (req, res) => {
-    const { id } = req.params;
-    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
-    todos.splice(todoIndex, 1);
+    const { id,todoIndex } = req.params;
+    const todoIndexs = todos.findIndex((t) => t.id === parseInt(id));
+    todos.splice(todoIndexs, 1);
     res.json(todos);
   });
 
   app.delete("/lab5/todos/:id", (req, res) => {
-    const { id } = req.params;
+    const { id, todoIndex } = req.params;
     if (todoIndex === -1) {
       res.status(404).json({ message: `Unable to delete Todo with ID ${id}` });
       return;
     }
-    const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
-    todos.splice(todoIndex, 1);
+    const todoIndexs = todos.findIndex((t) => t.id === parseInt(id));
+    todos.splice(todoIndexs, 1);
     res.sendStatus(200);
   });
 
@@ -99,11 +99,11 @@ export default function WorkingWithArrays(app) {
   });
 
   app.put("/lab5/todos/:id", (req, res) => {
-    const { id } = req.params;
+    const { id ,todoIndex} = req.params;
     if (todoIndex === -1) {
         res.status(404).json({ message: `Unable to update Todo with ID ${id}` });
         return;
-      }  
+      }   
     todos = todos.map((t) => {
       if (t.id === parseInt(id)) {
         return { ...t, ...req.body };
